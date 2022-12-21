f9214b33c48353a76cf37d39dd7ba4116f324f97
Aylmer Britto <aylmerbritto@Aylmers-MacBook-Air.local>
2022-12-20 19:33:32 -0500

    wip: android client initial commit

---
+++ android-client/app/src/main/java/edu/cmu/cs/openrtist/DressUpHomeActivity.java
@@ -0,0 +1,12 @@
+package edu.cmu.cs.openrtist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class DressUpHomeActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.dressup_home);
+    }
+}
